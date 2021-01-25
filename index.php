f5bddc36e90a15503247959ad904bfe3ac8eba80
Pierrickmartinet <pierrick.martinet@le-campus-numerique.fr>
2021-01-25 15:45:14 +0100

    v1.2

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
+
+//test
+
 echo "hello world yeah";
+
+// fonction filtre de nettoyage URL
+
+$pagefiltre = filter_input (INPUT_GET, 'action' ,FILTER_SANITIZE_STRING);
+
+
+// front controller
+
+if (isset ($pagefiltre)) {
+    if ($pagefiltre == 'cv') {
+        require 'action/cv.php';
+    } else if ($pagefiltre == 'hobbie') {
+        require 'action/hobbie.php';
+    } else if ($pagefiltre == 'contact') {
+        require 'action/contact.php';
+    } else {
+        require 'action/404.php';
+    }
+} else {
+    require 'action/cv.php';
+}
 ?>
