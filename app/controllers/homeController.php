3195811471e51adf4d45b9c67189c4246b862549
Pierrickmartinet <pierrick.martinet@le-campus-numerique.fr>
2021-01-26 15:53:13 +0100

    V 1.5.1 and V 1.5.2

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -1,2 +1,8 @@
 <?php
-echo "hello world";
+
+require 'app/persistences/blogPostData.php';
+
+// Affiche les 10 derniers post de la BDD blog
+
+$tenLastPost = lastBlogPosts($dbh);
+var_dump($tenLastPost);
---
+++ app/persistences/blogPostData.php
@@ -0,0 +1,7 @@
+<?php
+
+// Fonction qui retourne les dixs derniers posts de la BDD 'blog'
+
+function lastBlogPosts (PDO $connexion) {
+    return [];
+}
