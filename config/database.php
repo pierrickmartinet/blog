7ec5ec9a16a53bced59ec3530db1d611501db141
Pierrickmartinet <pierrick.martinet@le-campus-numerique.fr>
2021-01-26 13:51:29 +0100

    v 1.3

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+try {
+    $user = 'pierrick';
+    $pass = 'lacolombe02';
+    $dbh = new PDO('mysql:host=localhost;dbname=Blog;charset=UTF8', $user, $pass);
+} catch (PDOException $e) {
+echo "Erreur !: " . $e -> getMessage() ;
+die();
+}
--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
 
-//test
+require_once 'config/database.php'; // connecte à la BDD 'Blog' grâce à la configuration intégré dans le fichier database.php
 
-echo "hello world yeah";
+// front controller sous forme de tableau
 
-// fonction filtre de nettoyage URL
+$map = [
+    'home' => 'app/persistences/home.php',
+    '404' => 'ressources/views/errors/404.php',
+];
 
-$pagefiltre = filter_input (INPUT_GET, 'action' ,FILTER_SANITIZE_STRING);
-
-
-// front controller
-
-if (isset ($pagefiltre)) {
-    if ($pagefiltre == 'cv') {
-        require 'action/cv.php';
-    } else if ($pagefiltre == 'hobbie') {
-        require 'action/hobbie.php';
-    } else if ($pagefiltre == 'contact') {
-        require 'action/contact.php';
-    } else {
-        require 'action/404.php';
+if (filter_has_var(INPUT_GET, 'action')) { // lit si il y a une valeur aprés action dns l'url
+    $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING); // filtre l'url
+    if (!isset($map [$action])) { // Si Map n'est pas égale à une des pages du tableau alors 404
+        $action = '404';
     }
-} else {
-    require 'action/cv.php';
+} else { // sinon page accueil
+    $action = 'home';
 }
-?>
+
+// la variable fichier prend la valeur de la page du tableau correspondant à la page inscrite dans l'url aprés action
+
+$fichier = $map [$action];
+
+// Affiche la page
+
+require $fichier;
